a85c5f0f9e597c2aa65b0b47dac37a953ba94923
zarat <manuel.zarat@gmail.com>
2024-06-30 13:54:36 +0200

    Initial Commit

---
+++ face_recognition/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__author__ = """Manuel Zarat"""
+__email__ = 'manuel.zarat@gmail.com'
+__version__ = '1.0'
+
+from .api import load_image_file, face_locations, batch_face_locations, face_landmarks, face_encodings, compare_faces, face_distance
